970c1af91d9223849830a22db653afacf7c51bd2
dorisjenny27 <dorisjenny27@gmail.com>
2023-09-25 13:30:44 +0100

    Modification of my codes

---
+++ cier.py
@@ -0,0 +1,11 @@
+# Ask user for their name
+name = input("what's your name? ")
+
+# Remove Whitespace from strip
+name = name.strip()
+
+# Capitalize user's name
+name = name.capitalize()
+
+# Say hello to user
+print(f"hello, {name}")
